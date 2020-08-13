a4433993d3988bebf735a41825a2fc07ede038b6
inux <inux.steve@gmail.com>
2020-08-14 01:05:43 +0200

    📦 NEW: released first version 0.0.3

--- release.sh
+++ release.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+# Usage: ./release.sh nuget_api_key
 
 source env.sh
 
-dotnet publish -c Release $SERVER
+PACK_DIR="bin/Debug"
+NUGET_URL="https://api.nuget.org/v3/index.json"
+
+#Core
+(cd Markdoc.Core/src && 
+    rm -f $PACK_DIR/*.nupkg &&
+    dotnet pack && 
+    dotnet nuget push "$PACK_DIR"/Markdoc.Core.0.0.3.nupkg -k $1 -s $NUGET_URL)
+
+#Components
+(cd Markdoc.Components/src &&
+    rm -f $PACK_DIR/*.nupkg &&
+    dotnet pack &&
+    dotnet nuget push "$PACK_DIR"/Markdoc.Components.0.0.3.nupkg -k $1 -s $NUGET_URL)
 
 exit $?
